bc891850ce7bfa0b18a72c957b3eb93ed04e6409
Luis Reyes @lualreye <lualreye@gmail.com>
2025-02-10 16:55:33 -0500

    INITIAL COMMIT

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import Loader from './components/Loader.vue';
+
+const isLoading = ref(false)
+
+onMounted(() => {
+  isLoading.value = true
+})
+</script>
+
+<template>
+  <div class="app-container">
+    <Loader :isLoading="!isLoading"></Loader>
+      
+    <div v-if="isLoading" class="video-container">
+      <video width="320" height="240" controls>
+        <source src="./assets/invitation.mp4" type="video/mp4" />
+        Tu navegador no soporta la etiqueta de video.
+      </video>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.app-container {
+  background: #f6f2e5;
+  min-height: 100vh;
+  margin: auto 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.video-container {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  opacity: 0;
+  animation: fadeIn 2s ease-in-out 2s forwards;
+}
+
+@keyframes fadeIn {
+  from { opacity: 0; }
+  to { opacity: 1; }
+}
+</style>
---
+++ src/components/Loader.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+defineProps({
+  isLoading: {
+    type: Boolean,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <div v-if="isLoading" class="loader-container">
+    <div class="heart" />
+  </div>
+</template>
+
+<style scoped>
+* { margin: 0; padding: 0; box-sizing: border-box; }
+
+.loader-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: white;
+  z-index: 1000;
+}
+
+.heart {
+  position: relative;
+  width: 100px;
+  height: 100px;
+  background: transparent;
+  border: 5px solid green;
+  transform: rotate(-45deg);
+  animation: expand 2s forwards;
+}
+
+.heart::before, .heart::after {
+  content: "";
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  background: transparent;
+  border: 5px solid green;
+  border-radius: 50%;
+}
+
+.heart::before {
+  top: -50px;
+  left: 0;
+}
+
+.heart::after {
+  left: 50px;
+  top: 0;
+}
+
+@keyframes expand {
+  0% { transform: rotate(-45deg) scale(1); }
+  100% { transform: rotate(-45deg) scale(10); opacity: 0; }
+}
+</style>
